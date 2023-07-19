97e89fa5b27286e22baa8ab0ce43aedd9c21b3d5
Javier Sosa <jsosa@globaluy.com>
2023-07-18 22:44:25 -0300

    Add work-with-us section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,51 @@
-import * as React from "react"
-import "../styles/main.scss"
-import type { HeadFC, PageProps } from "gatsby"
+import * as React from "react";
+import "../styles/main.scss";
+import type { HeadFC, PageProps } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
 const pageStyles = {
   color: "#232129",
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+};
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
-}
+};
 const headingAccentStyles = {
   color: "#663399",
-}
+};
 const paragraphStyles = {
   marginBottom: 48,
-}
+};
 const codeStyles = {
   color: "#8A6534",
   padding: 4,
   backgroundColor: "#FFF4DB",
   fontSize: "1.25rem",
   borderRadius: 4,
-}
+};
 const listStyles = {
   marginBottom: 96,
   paddingLeft: 0,
-}
+};
 const doclistStyles = {
   paddingLeft: 0,
-}
+};
 const listItemStyles = {
   fontWeight: 300,
   fontSize: 24,
   maxWidth: 560,
   marginBottom: 30,
-}
+};
 
 const linkStyle = {
   color: "#8954A8",
   fontWeight: "bold",
   fontSize: 16,
   verticalAlign: "5%",
-}
+};
 
 const docLinkStyle = {
   ...linkStyle,
@@ -52,7 +53,7 @@ const docLinkStyle = {
   display: `inline-block`,
   marginBottom: 24,
   marginRight: 12,
-}
+};
 
 const descriptionStyle = {
   color: "#232129",
@@ -60,7 +61,7 @@ const descriptionStyle = {
   marginTop: 10,
   marginBottom: 0,
   lineHeight: 1.25,
-}
+};
 
 const docLinks = [
   {
@@ -72,8 +73,8 @@ const docLinks = [
     text: "GraphQL Typegen Documentation",
     url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
     color: "#8954A8",
-  }
-]
+  },
+];
 
 const badgeStyle = {
   color: "#fff",
@@ -89,7 +90,7 @@ const badgeStyle = {
   top: -2,
   marginLeft: 10,
   lineHeight: 1,
-}
+};
 
 const links = [
   {
@@ -135,60 +136,96 @@ const links = [
       "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
     color: "#663399",
   },
-]
+];
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+    <main>
+      <section
+        className="is-flex is-align-items-center is-justify-content-center p-4"
+        style={{ height: "760px" }}
+      >
+        <div
+          className="columns is-mobile is-multiline is-0 has-background-dark m-0 card-work-with-us"
+          style={{ borderRadius: "8px" }}
+        >
+          <div className="column is-12-mobile is-12-tablet is-6-desktop p-0 is-relative">
+            <StaticImage
+              src="../images/intersect.svg"
+              alt="Descripción de la imagen"
+            />
+            <StaticImage
+              src="../images/developer-boy.svg"
+              alt="Descripción de la imagen"
+              className="developer-boy-style"
+            />
+          </div>
+          <div className="column is-12-mobile is-12-tablet is-6-desktop p-4">
+            {/* <div className="p-4 has-background-dark"> */}
+            <div className="columns is-mobile mb-4">
+              <div className="column is-8-mobile is-12-tablet is-8-desktop pb-0">
+                <h1 className="title is-outfit is-4 is-size-3-tablet is-size-2-desktop has-text-primary">
+                  We are dev first, we care about you.
+                </h1>
+              </div>
+            </div>
+
+            <div className="columns is-mobile">
+              <div className="column is-12-mobile is-11-tablet is-9-desktop pb-0">
+                <p className="has-text-white is-size-4-desktop">
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                  diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                  aliquam erat volutpat.
+                </p>
+              </div>
+            </div>
+
+            <div className="container-button-work-with-us">
+              <button className="button is-primary custom-btn">
+                Trabaja con nosotros
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* </div> */}
+
+        {/* <div className="card" style={{ borderRadius: "8px" }}>
+          <div className="card-image">
+            
+            <div className="has-background-dark">
+              <StaticImage
+                src="../images/intersect.svg"
+                alt="Descripción de la imagen"
+              />
+            </div>
+          </div>
+          <div className="card-content p-4 has-background-dark">
+            <div className="columns is-mobile mb-4">
+              <div className="column is-8-mobile pb-0">
+                <h1 className="title is-outfit is-4 has-text-primary">
+                  We are dev first, we care about you.
+                </h1>
+              </div>
+            </div>
+
+            <p className="has-text-white">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
+              erat volutpat.
+            </p>
+
+            <div className="is-flex is-flex is-align-items-center is-justify-content-center my-5">
+              <button className="button is-primary custom-btn">
+                Trabaja con nosotros
+              </button>
+            </div>
+          </div>
+        </div> */}
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Home Page</title>;
