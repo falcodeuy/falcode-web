d7e0f7f9f3fdf30c4c681e8c049fce3e5b4ea174
Javier Sosa <jsosa@globaluy.com>
2023-07-23 20:01:17 -0300

    Add Footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="p-4 has-background-dark">
+      <div className="columns is-mobile is-multiline is-0 m-0 mb-6">
+        <div
+          className="column is-12-mobile is-4-tablet is-4-desktop pb-0"
+          style={{ marginBottom: "40px" }}
+        >
+          <StaticImage
+            src="../images/falcode-logo-footer.svg"
+            alt="Falcode footer logo"
+          />
+        </div>
+
+        <div
+          className="column is-12-mobile is-4-tablet is-4-desktop pb-0"
+          style={{ marginBottom: "40px" }}
+        >
+          <ul className="has-text-white">
+            <li style={{ marginBottom: "32px" }}>
+              <i className="fas fa-envelope mr-3"></i> correo@example.com
+            </li>
+            <li style={{ marginBottom: "32px" }}>
+              <i className="fas fa-phone mr-3"></i> +598 99 487 478
+            </li>
+            <li style={{ marginBottom: "32px" }}>
+              <i className="fas fa-map-marker-alt mr-3"></i> Montevideo, Uruguay
+            </li>
+          </ul>
+        </div>
+
+        <div className="column is-12-mobile is-4-tablet is-4-desktop pb-0">
+          <div className="field is-grouped is-grouped-multiline">
+            <div className="control">
+              <a
+                href="#"
+                className="icon is-medium mr-5 is-size-3 has-text-white"
+              >
+                <i className="fab fa-twitter"></i>
+              </a>
+            </div>
+            <div className="control">
+              <a
+                href="#"
+                className="icon is-medium mr-5 is-size-3 has-text-white"
+              >
+                <i className="fab fa-linkedin"></i>
+              </a>
+            </div>
+            <div className="control">
+              <a
+                href="#"
+                className="icon is-medium mr-5 is-size-3 has-text-white"
+              >
+                <i className="fab fa-facebook"></i>
+              </a>
+            </div>
+            <div className="control">
+              <a
+                href="#"
+                className="icon is-medium mr-5 is-size-3 has-text-white"
+              >
+                <i className="fab fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container-signature">
+        <p className="has-text-white">Falcode © 2023</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
