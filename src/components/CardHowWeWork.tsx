9f52647d9355d4b8c183447a6dbb8bf932831a90
Javier Sosa <jcarlos.sosagomez@gmail.com>
2023-07-30 20:00:00 -0300

    Add how-we-work section

--- src/components/CardHowWeWork.tsx
+++ src/components/CardHowWeWork.tsx
@@ -23,16 +23,11 @@ const CardHowWeWork: React.FC<CardHowWeWorkProps> = ({
         className="has-background-light"
         style={{ borderRadius: 8, position: "relative" }}
       >
-        {/* <StaticImage
-          src="../images/lightbulb.png"
-          alt="icon"
-          style={{ position: "absolute", top: -27, left: 32.66 }}
-        /> */}
         {image}
         <div className="p-5">
           <div className="columns is-mobile mt-5 mb-4">
-            <div className="column is-8-mobile is-8-tablet is-8-desktop py-0">
-              <h1 className="title is-outfit is-4 has-text-primary">{title}</h1>
+            <div className="column is-8-mobile is-8-tablet is-11-widescreen is-12-fullhd py-0">
+              <h1 className="title is-outfit has-text-primary title-cards-size">{title}</h1>
             </div>
           </div>
 
