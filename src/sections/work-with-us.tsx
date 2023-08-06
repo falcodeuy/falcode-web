61868e0937d1948e4845fe3bddc3e958ef59c6d7
Brian Stefanovich <stefanovich.brian@gmail.com>
2023-08-06 19:18:42 -0300

    Create work with us svg component

--- src/sections/work-with-us.tsx
+++ src/sections/work-with-us.tsx
@@ -1,90 +1,101 @@
-import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
+import React from "react";
 
-const WorkWithUs: React.FC = () => {
+export default function WorkWithUs() {
   return (
-    <section
-      className="is-flex is-align-items-center is-justify-content-center p-4"
-      style={{ height: "760px" }}
-    >
-      <div
-        className="columns is-mobile is-multiline is-0 has-background-dark m-0 card-work-with-us"
-        style={{ borderRadius: "8px" }}
+    <>
+      <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+        height="696"
+        viewBox="0 0 680 696"
+        className="card-work-with-us --mobile"
+        fill="none"
       >
-        <div className="column is-12-mobile is-12-tablet is-6-desktop p-0 is-relative">
+        <rect
+          width="680"
+          height="648"
+          className="card__background"
+          rx="8"
+          transform="matrix(1 0 0 -1 0 696)"
+          fill="#151427"
+        />
+        <path
+          d="M512.555 48H8C3.58172 48 0 51.5817 0 56V410H680C680 264.973 615.005 135.127 512.555 48Z"
+          fill="#E8536D"
+          className="card__highlight"
+        />
+        <foreignObject
+          key="image"
+          d="M49 0H483V410H49V0Z"
+          width="434"
+          height="410"
+        >
           <StaticImage
-            src="../images/intersect.svg"
-            alt="Descripción de la imagen"
+            className="card__image"
+            key="mobileImage"
+            src="../images/work-with-us-picture.png"
+            alt="Work with us"
           />
-          <StaticImage
-            src="../images/developer-boy.svg"
-            alt="Descripción de la imagen"
-            className="developer-boy-style"
-          />
-        </div>
-        <div className="column is-12-mobile is-12-tablet is-6-desktop p-4">
-          {/* <div className="p-4 has-background-dark"> */}
-          <div className="columns is-mobile mb-4">
-            <div className="column is-8-mobile is-12-tablet is-8-desktop pb-0">
-              <h1 className="title is-outfit is-4 is-size-3-tablet is-size-2-desktop has-text-primary">
-                We are dev first, we care about you.
-              </h1>
-            </div>
-          </div>
-
-          <div className="columns is-mobile">
-            <div className="column is-12-mobile is-11-tablet is-9-desktop pb-0">
-              <p className="has-text-white is-size-4-desktop">
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-                diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-                aliquam erat volutpat.
-              </p>
-            </div>
-          </div>
-
-          <div className="container-button-work-with-us">
+        </foreignObject>
+        <foreignObject key="content" x="30" y="439" width="620" height="228">
+          <div className="card_content">
+            <h1 className="title is-outfit is-4 is-size-3-tablet is-size-2-desktop has-text-primary">
+              We are dev first, we care about you.
+            </h1>
+            <p className="has-text-white is-size-4-desktop">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+              diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+              aliquam erat volutpat.
+            </p>
             <button className="button is-primary custom-btn">
               Trabaja con nosotros
             </button>
           </div>
-        </div>
-      </div>
-      {/* </div> */}
+        </foreignObject>
+      </svg>
 
-      {/* <div className="card" style={{ borderRadius: "8px" }}>
-          <div className="card-image">
-            
-            <div className="has-background-dark">
-              <StaticImage
-                src="../images/intersect.svg"
-                alt="Descripción de la imagen"
-              />
-            </div>
-          </div>
-          <div className="card-content p-4 has-background-dark">
-            <div className="columns is-mobile mb-4">
-              <div className="column is-8-mobile pb-0">
-                <h1 className="title is-outfit is-4 has-text-primary">
-                  We are dev first, we care about you.
-                </h1>
-              </div>
-            </div>
-
-            <p className="has-text-white">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
-              erat volutpat.
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 1427 664"
+        className="card-work-with-us --desktop"
+        fill="none">
+        <rect
+          className="card__background"
+          width="1414"
+          height="640"
+          rx="8"
+          transform="matrix(1 0 0 -1 13 664)"
+          fill="#151427"
+        />
+        <path
+          className="card__highlight"
+          d="M328.555 24H17C12.5818 24 9 27.5817 9 32V656C9 660.419 12.5817 664 17 664H568.49C571.724 634.783 573.395 604.946 573.395 574.626C573.395 342.761 475.672 139.225 328.555 24Z"
+          fill="#E8536D"
+        />
+        <foreignObject key="image" d="M0 0H658V664H0V0Z" width="658" height="664">
+          <StaticImage
+            key="desktopImage"
+            className="card__image"
+            src="../images/work-with-us-picture.png"
+            alt="Work with us"
+          />
+        </foreignObject>
+        <foreignObject key="content" x="732" y="141" width="571" height="406">
+          <div className="card_content">
+            <h1 className="title is-outfit is-4 is-size-3-tablet is-size-2-desktop has-text-primary">
+              We are dev first, we care about you.
+            </h1>
+            <p className="has-text-white is-size-4-desktop">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+              diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+              aliquam erat volutpat.
             </p>
-
-            <div className="is-flex is-flex is-align-items-center is-justify-content-center my-5">
-              <button className="button is-primary custom-btn">
-                Trabaja con nosotros
-              </button>
-            </div>
+            <button className="button is-primary custom-btn">
+              Trabaja con nosotros
+            </button>
           </div>
-        </div> */}
-    </section>
+        </foreignObject>
+      </svg>
+    </>
   );
 };
-
-export default WorkWithUs;
