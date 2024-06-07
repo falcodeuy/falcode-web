3b6e039b8dbffce64ec43c55c926e842240fcb5f
Nacho Fornier <nachofornier19@gmail.com>
2024-06-06 21:17:33 -0300

    wip

--- src/sections/how-we-work.tsx
+++ src/sections/how-we-work.tsx
@@ -4,120 +4,125 @@ import { StaticImage } from "gatsby-plugin-image";
 
 const HowWeWork: React.FC = () => {
   return (
-    <section className="">
-      <div className="section-padding">
-        <h1 className="title-how-we-work">¿Cómo trabajamos en Falcode?</h1>
-        <h1 className="title is-outfit has-text-dark has-text-centered mb-6 title-secondary-size">
+    <section className=''>
+      <div className='section-padding'>
+        <h1 className='title-how-we-work'>¿Cómo trabajamos en Falcode?</h1>
+        <h1 className='title is-outfit has-text-dark has-text-centered mb-6 title-secondary-size'>
           Buscamos ser{" "}
-          <span className="has-text-primary">socios tecnológicos</span>, te
+          <span className='has-text-primary'>socios tecnológicos</span>, te
           acompañamos en cada etapa del proyecto y más allá
         </h1>
 
-        <div className="columns is-mobile is-multiline container-cards my-0">
+        <div className='columns is-mobile is-multiline container-cards my-0'>
           <CardHowWeWork
             title={"Ideamos la mejor estrategia"}
             content={
-              "Phasellus euismod diam ut quam dictum dignissim. Vivamus scelerisque nisl nisl, in bibendum erat dictum sit amet."
+              "Cada plan que diseñamos está cuidadosamente elaborado para aprovechar tus fortalezas y trabajamos contigo para entender tus necesidades, garantizando que siempre estés un paso adelante en tu camino hacia el logro de tus objetivos."
             }
             image={
               <StaticImage
-                className="badge-card-position"
-                src="../images/lightbulb.png"
-                alt="icon"
+                className='badge-card-position'
+                src='../images/lightbulb.png'
+                alt='icon'
               />
             }
           />
           <CardHowWeWork
             title={"Aseguramos la calidad"}
             content={
-              "Phasellus euismod diam ut quam dictum dignissim. Vivamus scelerisque nisl nisl, in bibendum erat dictum sit amet."
+              "Contamos con un equipo de expertos altamente capacitados que trabajan rigurosamente para garantizar que cada solución cumpla con los más altos estándares de calidad."
             }
             image={
               <StaticImage
-                className="badge-card-position"
-                src="../images/badge.png"
-                alt="icon"
+                className='badge-card-position'
+                src='../images/badge.png'
+                alt='icon'
               />
             }
           />
           <CardHowWeWork
             title={"Seleccionamos el talento"}
             content={
-              "Phasellus euismod diam ut quam dictum dignissim. Vivamus scelerisque nisl nisl, in bibendum erat dictum sit amet."
+              "Nuestra empresa se enorgullece de ser líderes en el desarrollo de soluciones innovadoras para nuestros clientes. Con nuestro enfoque creativo y nuestros años de experiencia, aseguramos que nuestra solución cumplirá con todas tus necesidades y expectativas."
             }
             image={
               <StaticImage
-                className="badge-card-position"
-                src="../images/sniper.png"
-                alt="icon"
+                className='badge-card-position'
+                src='../images/sniper.png'
+                alt='icon'
               />
             }
           />
           <CardHowWeWork
             title={"Creamos soluciones top"}
             content={
-              "Phasellus euismod diam ut quam dictum dignissim. Vivamus scelerisque nisl nisl, in bibendum erat dictum sit amet."
+              "Nuestra empresa se enorgullece de ser líderes en el desarrollo de soluciones innovadoras para nuestros clientes. Con nuestro enfoque creativo y nuestros años de experiencia, aseguramos que nuestra solución cumplirá con todas tus necesidades y expectativas."
             }
             image={
               <StaticImage
-                className="badge-card-position"
-                src="../images/rocket.png"
-                alt="icon"
+                className='badge-card-position'
+                src='../images/rocket.png'
+                alt='icon'
               />
             }
           />
         </div>
 
-        <div className="has-background-light divider"></div>
+        <div className='has-background-light divider'></div>
 
-        <h1 className="title is-outfit has-text-dark title-type-project title-secondary-size">
-          Como <span className="has-text-primary">socios tecnológicos</span>{" "}
+        <h1 className='title is-outfit has-text-dark title-type-project title-secondary-size'>
+          Como <span className='has-text-primary'>socios tecnológicos</span>{" "}
           ofrecemos nuestra habilidad para planificar, organizar y construir
           proyectos innovadores de alto impacto.
         </h1>
 
         <div>
-          <h1 className="is-size-6 mb-4">Nuestras soluciones abarcan:</h1>
+          <h1 className='is-size-6 mb-4'>Nuestras soluciones abarcan:</h1>
 
-          <div className="columns is-mobile is-multiline">
-            <div className="column is-narrow">
-              <span className="tag is-dark is-rounded is-size-6">
+          <div className='columns is-mobile is-multiline'>
+            <div className='column is-narrow'>
+              <span className='tag is-dark is-rounded is-size-6'>
                 Desarrollo Web
               </span>
             </div>
-            <div className="column is-narrow">
-              <span className="tag is-dark is-rounded is-size-6">
+            <div className='column is-narrow'>
+              <span className='tag is-dark is-rounded is-size-6'>
                 Aplicaciones Móbiles
               </span>
             </div>
-            <div className="column is-narrow">
-              <span className="tag is-dark is-rounded is-size-6">
-                Aplicaciones Móbiles
+            <div className='column is-narrow'>
+              <span className='tag is-dark is-rounded is-size-6'>
+                Inteligencia artificial
               </span>
             </div>
-            <div className="column is-narrow">
-              <span className="tag is-dark is-rounded is-size-6">
-                Consultoría
+            <div className='column is-narrow'>
+              <span className='tag is-dark is-rounded is-size-6'>
+                Procesamientos de imágenes
               </span>
             </div>
-            <div className="column is-narrow">
-              <span className="tag is-dark is-rounded is-size-6">
-                Consultoría
+            <div className='column is-narrow'>
+              <span className='tag is-dark is-rounded is-size-6'>
+                Analisis de datos
               </span>
             </div>
-            <div className="column is-narrow">
-              <span className="tag is-dark is-rounded is-size-6">
-                Desarrollo Web
+            <div className='column is-narrow'>
+              <span className='tag is-dark is-rounded is-size-6'>
+                Gestión de proyectos
               </span>
             </div>
-            <div className="column is-narrow">
-              <span className="tag is-dark is-rounded is-size-6">
-                Aplicaciones Móbiles
+            <div className='column is-narrow'>
+              <span className='tag is-dark is-rounded is-size-6'>
+                Tecnologias en la nube
               </span>
             </div>
-            <div className="column is-narrow">
-              <span className="tag is-dark is-rounded is-size-6">
-                Desarrollo Web
+            <div className='column is-narrow'>
+              <span className='tag is-dark is-rounded is-size-6'>
+                Gestión de infracstructura
+              </span>
+            </div>
+            <div className='column is-narrow'>
+              <span className='tag is-dark is-rounded is-size-6'>
+                Optimización de procesos
               </span>
             </div>
           </div>
--- src/sections/work-with-us.tsx
+++ src/sections/work-with-us.tsx
@@ -3,53 +3,54 @@ import React from "react";
 
 export default function WorkWithUs() {
   return (
-    <div className="section-padding">
+    <div className='section-padding'>
       <svg
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        height="696"
-        viewBox="0 0 680 696"
-        className="card-work-with-us --mobile"
-        fill="none"
+        xmlns='http://www.w3.org/2000/svg'
+        xmlnsXlink='http://www.w3.org/1999/xlink'
+        height='696'
+        viewBox='0 0 680 696'
+        className='card-work-with-us --mobile'
+        fill='none'
       >
         <rect
-          width="680"
-          height="648"
-          className="card__background"
-          rx="8"
-          transform="matrix(1 0 0 -1 0 696)"
-          fill="#151427"
+          width='680'
+          height='648'
+          className='card__background'
+          rx='8'
+          transform='matrix(1 0 0 -1 0 696)'
+          fill='#151427'
         />
         <path
-          d="M512.555 48H8C3.58172 48 0 51.5817 0 56V410H680C680 264.973 615.005 135.127 512.555 48Z"
-          fill="#E8536D"
-          className="card__highlight"
+          d='M512.555 48H8C3.58172 48 0 51.5817 0 56V410H680C680 264.973 615.005 135.127 512.555 48Z'
+          fill='#E8536D'
+          className='card__highlight'
         />
         <foreignObject
-          key="image"
-          d="M49 0H483V410H49V0Z"
-          width="434"
-          height="410"
+          key='image'
+          d='M49 0H483V410H49V0Z'
+          width='434'
+          height='410'
         >
           <StaticImage
-            className="card__image"
-            key="mobileImage"
-            src="../images/work-with-us-picture.png"
-            alt="Work with us"
+            className='card__image'
+            key='mobileImage'
+            src='../images/work-with-us-picture.png'
+            alt='Work with us'
           />
         </foreignObject>
-        <foreignObject key="content" x="30" y="439" width="620" height="228">
-          <div className="card_content">
-            <h1 className="title is-outfit is-4 is-size-3-tablet is-size-2-desktop has-text-primary">
+        <foreignObject key='content' x='30' y='439' width='620' height='228'>
+          <div className='card_content'>
+            <h1 className='title is-outfit is-4 is-size-3-tablet is-size-2-desktop has-text-primary'>
               We are dev first, we care about you.
             </h1>
-            <p className="has-text-white is-size-4-desktop">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
-              erat volutpat.
+            <p className='has-text-white is-size-4-desktop'>
+              Entendemos que los verdaderos protagonistas de la tecnología son
+              los desarrolladores. Somos un equipo apasionado de creadores y
+              soñadores, y estamos buscando a personas excepcionales como tú
+              para unirse a nuestro equipo.
             </p>
-            <div className="container-button-work-with-us">
-              <button className="button is-primary custom-btn">
+            <div className='container-button-work-with-us'>
+              <button className='button is-primary custom-btn'>
                 Trabaja con nosotros
               </button>
             </div>
@@ -58,50 +59,51 @@ export default function WorkWithUs() {
       </svg>
 
       <svg
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox="0 0 1427 664"
-        className="card-work-with-us --desktop"
-        fill="none"
+        xmlns='http://www.w3.org/2000/svg'
+        xmlnsXlink='http://www.w3.org/1999/xlink'
+        viewBox='0 0 1427 664'
+        className='card-work-with-us --desktop'
+        fill='none'
       >
         <rect
-          className="card__background"
-          width="1414"
-          height="640"
-          rx="8"
-          transform="matrix(1 0 0 -1 13 664)"
-          fill="#151427"
+          className='card__background'
+          width='1414'
+          height='640'
+          rx='8'
+          transform='matrix(1 0 0 -1 13 664)'
+          fill='#151427'
         />
         <path
-          className="card__highlight"
-          d="M328.555 24H17C12.5818 24 9 27.5817 9 32V656C9 660.419 12.5817 664 17 664H568.49C571.724 634.783 573.395 604.946 573.395 574.626C573.395 342.761 475.672 139.225 328.555 24Z"
-          fill="#E8536D"
+          className='card__highlight'
+          d='M328.555 24H17C12.5818 24 9 27.5817 9 32V656C9 660.419 12.5817 664 17 664H568.49C571.724 634.783 573.395 604.946 573.395 574.626C573.395 342.761 475.672 139.225 328.555 24Z'
+          fill='#E8536D'
         />
         <foreignObject
-          key="image"
-          d="M0 0H658V664H0V0Z"
-          width="658"
-          height="664"
+          key='image'
+          d='M0 0H658V664H0V0Z'
+          width='658'
+          height='664'
         >
           <StaticImage
-            key="desktopImage"
-            className="card__image"
-            src="../images/work-with-us-picture.png"
-            alt="Work with us"
+            key='desktopImage'
+            className='card__image'
+            src='../images/work-with-us-picture.png'
+            alt='Work with us'
           />
         </foreignObject>
-        <foreignObject key="content" x="732" y="141" width="571" height="406">
-          <div className="card_content">
-            <h1 className="title is-outfit is-4 is-size-3-tablet is-size-2-desktop has-text-primary">
+        <foreignObject key='content' x='732' y='141' width='571' height='406'>
+          <div className='card_content'>
+            <h1 className='title is-outfit is-4 is-size-3-tablet is-size-2-desktop has-text-primary'>
               We are dev first, we care about you.
             </h1>
-            <p className="has-text-white is-size-4-desktop">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
-              erat volutpat.
+            <p className='has-text-white is-size-4-desktop'>
+              Entendemos que los verdaderos protagonistas de la tecnología son
+              los desarrolladores. Somos un equipo apasionado de creadores y
+              soñadores, y estamos buscando a personas excepcionales como tú
+              para unirse a nuestro equipo.
             </p>
-            <div className="container-button-work-with-us">
-              <button className="button is-primary custom-btn">
+            <div className='container-button-work-with-us'>
+              <button className='button is-primary custom-btn'>
                 Trabaja con nosotros
               </button>
             </div>
