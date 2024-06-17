3bb6e3bfcdefd790cdc18925c64ad8e3a12d1764
Nacho Fornier <nachofornier19@gmail.com>
2024-06-17 19:22:16 -0300

    How we work and splash translations

--- src/sections/how-we-work.tsx
+++ src/sections/how-we-work.tsx
@@ -1,24 +1,41 @@
 import React from "react";
 import CardHowWeWork from "../components/card-how-we-work";
 import { StaticImage } from "gatsby-plugin-image";
+import { useIntl } from "gatsby-plugin-intl";
 
 const HowWeWork: React.FC = () => {
+  const intl = useIntl();
+
+  const tags = [
+    intl.formatMessage({ id: "tags.tag1" }),
+    intl.formatMessage({ id: "tags.tag2" }),
+    intl.formatMessage({ id: "tags.tag3" }),
+    intl.formatMessage({ id: "tags.tag4" }),
+    intl.formatMessage({ id: "tags.tag5" }),
+    intl.formatMessage({ id: "tags.tag6" }),
+    intl.formatMessage({ id: "tags.tag7" }),
+    intl.formatMessage({ id: "tags.tag8" }),
+    intl.formatMessage({ id: "tags.tag9" }),
+  ];
+
   return (
     <section className=''>
       <div className='section-padding'>
-        <h1 className='title-how-we-work'>¿Cómo trabajamos en Falcode?</h1>
+        <h1 className='title-how-we-work'>
+          {intl.formatMessage({ id: "howWeWorkTitle" })}
+        </h1>
         <h1 className='title is-outfit has-text-dark has-text-centered mb-6 title-secondary-size'>
-          Buscamos ser{" "}
-          <span className='has-text-primary'>socios tecnológicos</span>, te
-          acompañamos en cada etapa del proyecto y más allá
+          {intl.formatMessage({ id: "subTitle" })}
+          <span className='has-text-primary'>
+            {intl.formatMessage({ id: "subTitleHighlight" })}
+          </span>
+          {intl.formatMessage({ id: "subTitleContinuation" })}
         </h1>
 
         <div className='columns is-mobile is-multiline my-0'>
           <CardHowWeWork
-            title={"Ideamos la mejor estrategia"}
-            content={
-              "Cada plan que diseñamos está cuidadosamente elaborado para aprovechar tus fortalezas y trabajamos contigo para entender tus necesidades, garantizando que siempre estés un paso adelante en tu camino hacia el logro de tus objetivos."
-            }
+            title={intl.formatMessage({ id: "card1Title" })}
+            content={intl.formatMessage({ id: "card1Content" })}
             image={
               <StaticImage
                 className='badge-card-position'
@@ -28,10 +45,8 @@ const HowWeWork: React.FC = () => {
             }
           />
           <CardHowWeWork
-            title={"Aseguramos la calidad"}
-            content={
-              "Contamos con un equipo de expertos altamente capacitados que trabajan rigurosamente para garantizar que cada solución cumpla con los más altos estándares de calidad."
-            }
+            title={intl.formatMessage({ id: "card2Title" })}
+            content={intl.formatMessage({ id: "card2Content" })}
             image={
               <StaticImage
                 className='badge-card-position'
@@ -41,10 +56,8 @@ const HowWeWork: React.FC = () => {
             }
           />
           <CardHowWeWork
-            title={"Seleccionamos el talento"}
-            content={
-              "Nuestra empresa se enorgullece de ser líderes en el desarrollo de soluciones innovadoras para nuestros clientes. Con nuestro enfoque creativo y nuestros años de experiencia, aseguramos que nuestra solución cumplirá con todas tus necesidades y expectativas."
-            }
+            title={intl.formatMessage({ id: "card3Title" })}
+            content={intl.formatMessage({ id: "card3Content" })}
             image={
               <StaticImage
                 className='badge-card-position'
@@ -54,10 +67,8 @@ const HowWeWork: React.FC = () => {
             }
           />
           <CardHowWeWork
-            title={"Creamos soluciones top"}
-            content={
-              "Nuestra empresa se enorgullece de ser líderes en el desarrollo de soluciones innovadoras para nuestros clientes. Con nuestro enfoque creativo y nuestros años de experiencia, aseguramos que nuestra solución cumplirá con todas tus necesidades y expectativas."
-            }
+            title={intl.formatMessage({ id: "card4Title" })}
+            content={intl.formatMessage({ id: "card4Content" })}
             image={
               <StaticImage
                 className='badge-card-position'
@@ -71,60 +82,24 @@ const HowWeWork: React.FC = () => {
         <div className='has-background-light divider'></div>
 
         <h1 className='title is-outfit has-text-dark title-type-project title-secondary-size'>
-          Como <span className='has-text-primary'>socios tecnológicos</span>{" "}
-          ofrecemos nuestra habilidad para planificar, organizar y construir
-          proyectos innovadores de alto impacto.
+          {intl.formatMessage({ id: "offerTitle" })}
+          <span className='has-text-primary'>
+            {intl.formatMessage({ id: "offerTitleHighlight" })}
+          </span>
+          {intl.formatMessage({ id: "offerTitleContinuation" })}
         </h1>
 
         <div>
-          <h1 className='is-size-6 mb-4'>Nuestras soluciones abarcan:</h1>
+          <h1 className='is-size-6 mb-4'>
+            {intl.formatMessage({ id: "solutions" })}
+          </h1>
 
           <div className='columns is-mobile is-multiline'>
-            <div className='column is-narrow'>
-              <span className='tag is-dark is-rounded is-size-6'>
-                Desarrollo Web
-              </span>
-            </div>
-            <div className='column is-narrow'>
-              <span className='tag is-dark is-rounded is-size-6'>
-                Aplicaciones Móbiles
-              </span>
-            </div>
-            <div className='column is-narrow'>
-              <span className='tag is-dark is-rounded is-size-6'>
-                Inteligencia artificial
-              </span>
-            </div>
-            <div className='column is-narrow'>
-              <span className='tag is-dark is-rounded is-size-6'>
-                Procesamientos de imágenes
-              </span>
-            </div>
-            <div className='column is-narrow'>
-              <span className='tag is-dark is-rounded is-size-6'>
-                Analisis de datos
-              </span>
-            </div>
-            <div className='column is-narrow'>
-              <span className='tag is-dark is-rounded is-size-6'>
-                Gestión de proyectos
-              </span>
-            </div>
-            <div className='column is-narrow'>
-              <span className='tag is-dark is-rounded is-size-6'>
-                Tecnologias en la nube
-              </span>
-            </div>
-            <div className='column is-narrow'>
-              <span className='tag is-dark is-rounded is-size-6'>
-                Gestión de infracstructura
-              </span>
-            </div>
-            <div className='column is-narrow'>
-              <span className='tag is-dark is-rounded is-size-6'>
-                Optimización de procesos
-              </span>
-            </div>
+            {tags.map((tag: string, index: number) => (
+              <div className='column is-narrow' key={index}>
+                <span className='tag is-dark is-rounded is-size-6'>{tag}</span>
+              </div>
+            ))}
           </div>
         </div>
       </div>
