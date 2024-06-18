d48fa3ffbdd6b9f1b264565aec9dcb8a11edbb9b
Nacho Fornier <nachofornier19@gmail.com>
2024-06-18 11:13:47 -0300

    Remove phone number and added github to footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,75 +3,56 @@ import { StaticImage } from "gatsby-plugin-image";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="p-4 has-background-dark">
-      <div className="columns is-mobile is-multiline is-0 m-0 mb-6">
+    <footer className='p-4 has-background-dark'>
+      <div className='columns is-mobile is-multiline is-0 m-0 mb-6'>
         <div
-          className="column is-12-mobile is-4-tablet is-4-desktop pb-0"
+          className='column is-12-mobile is-4-tablet is-4-desktop pb-0'
           style={{ marginBottom: "40px" }}
         >
           <StaticImage
-            src="../images/falcode-logo-footer.svg"
-            alt="Falcode footer logo"
+            src='../images/falcode-logo-footer.svg'
+            alt='Falcode footer logo'
           />
         </div>
 
         <div
-          className="column is-12-mobile is-4-tablet is-4-desktop pb-0"
+          className='column is-12-mobile is-4-tablet is-4-desktop pb-0'
           style={{ marginBottom: "40px" }}
         >
-          <ul className="has-text-white">
+          <ul className='has-text-white'>
             <li style={{ marginBottom: "32px" }}>
-              <i className="fas fa-envelope mr-3"></i> correo@example.com
+              <i className='fas fa-envelope mr-3'></i> correo@example.com
             </li>
             <li style={{ marginBottom: "32px" }}>
-              <i className="fas fa-phone mr-3"></i> +598 99 487 478
-            </li>
-            <li style={{ marginBottom: "32px" }}>
-              <i className="fas fa-map-marker-alt mr-3"></i> Montevideo, Uruguay
+              <i className='fas fa-map-marker-alt mr-3'></i> Montevideo, Uruguay
             </li>
           </ul>
         </div>
 
-        <div className="column is-12-mobile is-4-tablet is-4-desktop pb-0">
-          <div className="field is-grouped is-grouped-multiline">
-            <div className="control">
+        <div className='column is-12-mobile is-4-tablet is-4-desktop pb-0'>
+          <div className='field is-grouped is-grouped-multiline'>
+            <div className='control'>
               <a
-                href="#"
-                className="icon is-medium mr-5 is-size-3 has-text-white"
+                href='https://github.com/falcodeuy'
+                className='icon is-medium mr-5 is-size-3 has-text-white'
               >
-                <i className="fab fa-twitter"></i>
+                <i className='fab fa-github'></i>
               </a>
             </div>
-            <div className="control">
+            <div className='control'>
               <a
-                href="#"
-                className="icon is-medium mr-5 is-size-3 has-text-white"
+                href='https://www.linkedin.com/company/falcode'
+                className='icon is-medium mr-5 is-size-3 has-text-white'
               >
-                <i className="fab fa-linkedin"></i>
-              </a>
-            </div>
-            <div className="control">
-              <a
-                href="#"
-                className="icon is-medium mr-5 is-size-3 has-text-white"
-              >
-                <i className="fab fa-facebook"></i>
-              </a>
-            </div>
-            <div className="control">
-              <a
-                href="#"
-                className="icon is-medium mr-5 is-size-3 has-text-white"
-              >
-                <i className="fab fa-instagram"></i>
+                <i className='fab fa-linkedin'></i>
               </a>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="container-signature">
-        <p className="has-text-white">Falcode © 2023</p>
+      <div className='container-signature'>
+        <p className='has-text-white'>Falcode © 2023</p>
       </div>
     </footer>
   );
