7b946f888a10cf123c1f957b588ae4a59087da0b
Fausto Márquez <fausto_7m@hotmail.com>
2026-01-03 18:44:03 -0300

    Add 'Our Toolbox' title to Technologies section, update technology icons to include names, and enhance styles for better layout and responsiveness.

--- src/sections/technologies.tsx
+++ src/sections/technologies.tsx
@@ -1,24 +1,48 @@
 import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
+import { useIntl } from "gatsby-plugin-intl";
 
 let technologies = [
-  "devicon-react-original",
-  "devicon-typescript-plain",
-  "devicon-nodejs-original",
-  "devicon-python-original",
-  "devicon-django-original",
-  "devicon-docker-original",
-  "devicon-amazonwebservices-original-wordmark",
-  "devicon-postgresql-original",
-  "devicon-djangorest-original",
-  "devicon-graphql-plain",
-  "devicon-nextjs-plain",
-  "devicon-gatsby-original",
+  { icon: "devicon-react-original", name: "React" },
+  { icon: "devicon-typescript-plain", name: "TypeScript" },
+  { icon: "devicon-nodejs-plain", name: "Node.js" },
+  { icon: "devicon-python-plain", name: "Python" },
+  { icon: "devicon-django-plain", name: "Django" },
+  { icon: "devicon-docker-plain", name: "Docker" },
+  { icon: "devicon-amazonwebservices-plain-wordmark", name: "AWS" },
+  { icon: "devicon-htmx-plain", name: "HTMX" },
+  { icon: "devicon-postgresql-plain", name: "PostgreSQL" },
+  { icon: "devicon-djangorest-plain-wordmark", name: "DRF" },
+  { icon: "devicon-flask-original", name: "Flask" },
+  { icon: "devicon-portainer-plain", name: "Portainer" },
+  { icon: "devicon-harbor-plain", name: "Harbor" },
+  { icon: "devicon-chartjs-plain", name: "Chart.js" },
+  { icon: "devicon-cloudflare-plain", name: "Cloudflare" },
+  { icon: "devicon-nextjs-plain", name: "Next.js" },
+  { icon: "devicon-gatsby-plain", name: "Gatsby" },
+  { icon: "devicon-linux-plain", name: "Linux" },
+  { icon: "devicon-astro-plain", name: "Astro" },
+  { icon: "devicon-tailwindcss-plain", name: "Tailwind" },
+  { icon: "devicon-html5-plain", name: "HTML5" },
+  { icon: "devicon-sass-plain", name: "Sass" },
+  { icon: "devicon-pytorch-original", name: "PyTorch" },
+  { icon: "devicon-materialui-plain", name: "MUI" },
+  { icon: "devicon-bulma-plain", name: "Bulma" },
+  { icon: "devicon-bash-plain", name: "Bash" },
+  { icon: "devicon-bootstrap-plain", name: "Bootstrap" },
+  { icon: "devicon-jquery-plain", name: "jQuery" },
+  { icon: "devicon-css3-plain", name: "CSS3" },
+  { icon: "devicon-javascript-plain", name: "JavaScript" },
+  { icon: "devicon-oracle-original", name: "Oracle" },
+  { icon: "devicon-mysql-plain", name: "MySQL" },
+  { icon: "devicon-android-plain", name: "Android" },
 ];
 
 technologies = technologies.concat(technologies);
 
 const Technologies: React.FC = () => {
+  const intl = useIntl();
+
   return (
     <section className="technologies-section">
       <StaticImage
@@ -28,11 +52,15 @@ const Technologies: React.FC = () => {
         layout="fullWidth"
       />
       <div className="technologies-carousel-wrapper">
+        <h2 className="technologies-title" data-aos="fade-in">
+          {intl.formatMessage({ id: "ourToolbox" })}
+        </h2>
         <div className="technologies-carousel">
           <div className="technologies-track">
             {[...technologies, ...technologies].map((tech, index) => (
-              <div key={`${tech}-${index}`} className="technology-icon">
-                <i className={tech}></i>
+              <div key={`${tech.icon}-${index}`} className="technology-icon">
+                <i className={tech.icon}></i>
+                <span className="technology-name">{tech.name}</span>
               </div>
             ))}
           </div>
